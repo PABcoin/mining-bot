150ed3b011ed780d2f78edf47100dd881f8c313e
pab <38088977+PABcoin@users.noreply.github.com>
2026-05-19 12:38:45 +0700

    Add files via upload

---
+++ client/src/App.jsx
@@ -0,0 +1,26 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import Login from './pages/Login'
+import Dashboard from './pages/Dashboard'
+
+function PrivateRoute({ children }) {
+  const token = localStorage.getItem('mining_token')
+  return token ? children : <Navigate to="/login" replace />
+}
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <div className="scanlines">
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/dashboard" element={
+            <PrivateRoute>
+              <Dashboard />
+            </PrivateRoute>
+          } />
+          <Route path="*" element={<Navigate to="/login" replace />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  )
+}
